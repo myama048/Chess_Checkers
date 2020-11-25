968007f5c03b014e452bb9747e09460bd6b1e05e
myama048 <myama048@ucr.edu>
2020-11-25 05:41:27 -0800

    implemented straight.hpp

--- MoveInterface.hpp
+++ MoveInterface.hpp
@@ -1,19 +1,13 @@
 #ifndef __MOVE_INTERFACE_HPP__
 #define __MOVE_INTERFACE_HPP__
 
-
+#include<vector>
+#include<iostream>
+#include<utility>
 
 class MoveInterface {
 	public:
-		int move(vector<vector<pair<int, int>>>, Pieces* p, int sx, int sy, int dx, int dy){
-	// check if spot if valid
-	int new_idx = 10 * dx + dy;
-	if(board->getStatus(new_idx) == 0){
-		return 1;
-	}
-	if(p->user == 0 && board->getStatus(new_idx) == 1){
-		return 2;
-	}
+		virtual bool move(std::vector<std::vector<std::pair<int, int>>> Board, std::vector<Pieces*> v_p, int sx, int sy, int dx, int dy) = 0;
 };
 
 
--- straightMove.hpp
+++ straightMove.hpp
@@ -7,20 +7,51 @@ class MoveInterface;
 
 class StraightMove : public MoveInterface {
 	public:
-		bool move(vector<vector<pair<int posiStatus, int idxPieceVector>>>, vector<Pieces*> v_p, int sx, int sy, int dx, int dy){
+		bool move(std::vector<std::vector<std::pair<int posiStatus, int idxPieceVector>>> Board, std::vector<Pieces*> v_p, int sx, int sy, int dx, int dy){
 			//int user = v_p[idxPieceVector]->get_user(); // need to get user
 			if(sx != dx) //column should be the same
 				return false;
 			if(sy == dy) // move is not done
 				return false;
-			for(int i = sy + 1; i < dy - 1; i++){ // loop to check all spots in between sy & (dy - 1) are open
-				if(Board[sx][i].first != 1) // 1 means the board is empty
-					return false;
-			}	
 			
-			if(Board[sx][dy] == 2){ // 2 means that there's enemy's piece
+			if(Board[sx][sy].first == 1){ //player 1 's POV
+				for(int i = sy + 1; i < dy - 1; i++){ // loop to check all spots in between sy & (dy - 1) are open
+					if(Board[sx][i].first != 0) // 0 means the board is empty
+						return false;
+				}
+	
+				if(Board[dx][dy].first == 1){ // there's other piece on [dx][dy]
+					return false;		// 1 means your piece
+				}	
 					
+				if(Board[dx][dy].first == 2){ // 2 means that there's enemy's piece
+					vp[Board[sx][sy].second]->setStatus(false);// set status of enemy's piece to be taken
+				}
+
+				Board[sx][sy].first = 0; // update the start to be empty 0 = empty
+				Board[dx][dy].first = 1;// new location to be occupied
 			}
+			else if(Board[sx][sy].first == 2){ // player 2 's POV
+				new_sx = 9 - sx;
+				new_sy = 9 - sy;
+				new_dx = 9 - dx;
+				new_dy = 9 - dy;
+				for(int i = new_sy - 1; i > dy + 1; i--){
+					if(Board[new_sx][i].first != 0)
+						return false;
+				}
+
+				if(Board[new_dx][new_dy].first == 2)
+					return false;
+
+				if(Board[new_dx][new_dy].first == 1){
+					vp[Board[sx][sy].second]->setStatus(false);
+				}
+
+				Board[new_sx][new_sy].first = 0;
+				Board[new_dx][new_dy] = 2;
+			}
+			return true;
 		}
 };
 

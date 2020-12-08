e99076effffb1d525ae1dc45c5ac22a304b727e8
myama048 <myama048@ucr.edu>
2020-12-08 14:16:57 -0800

    updated checkerMove

--- checkerMove.hpp
+++ checkerMove.hpp
@@ -12,52 +12,38 @@ class CheckerMove : public MoveInterface {
 	public:
 		bool move(std::vector<std::vector<std::pair<int, int>>> &Board, std::vector<Piece*> v_p, int sy, int sx, int dy, int dx){
 			
-			if(sx == dx || sy == dy){ 
+			if(abs(dx - sx) != abs(dy - sy)){ 
 				//printf("Invalid move\n");
 				return false;
 			}
-			if(dx - sx > 2 || dy - sy > 2){
-				return false;
-			}
-			if(Board[dy][dx].first != 0){
-			//	printf("dest not empty\n");
-				return false;
-			}
+		
 			if(!(sx >= 0 && sx <= 7 && sy >= 0 && sy <= 7 && dx >= 0 && dx <= 7 && dy >= 0 && dy <= 7)){
 				//printf("not in range\n");
 				return false;
 			}
 
-			if(sx < dx) { //moving to the upper right	
-				//printf("right\n");
-				if(dy - sy == 2 && dx - sx == 2){// jumping over
-					if(Board[sy + 1][sx + 1].first == 0){
+			if(abs(dx - sx) == 2) { // jumping
+				int midx = (sx + dx) / 2;
+				int midy = (sy + dy) / 2;
+					if(Board[midy][midx].first == 0){
 						return false; // can't jump over empty
 					}
-					else if(Board[sy + 1][sx + 1].first == Board[sy][sx].first){
+					else if(Board[midy][midx].first == Board[sy][sx].first){
 						return false; // can't jump over your piece
 					}
 					else{
-						v_p[Board[sy+1][sx+1].second]->setStatus(false);
-						Board[sy+1][sx+1].first = 0;
+						v_p[Board[midy][midx].second]->setStatus(false);
+						Board[midy][midx].first = 0;
 					}
+			}
+			else if(abs(dx - sx) == 1){ // moving to the upper left
+				if(Board[dy][dx].first != 0){
+					return false;
 				}
 			}
-			else if(sx > dx){ // moving to the upper left
-				//printf("left\n");
-				if(dy - sy == 2 && sx - dy == 2){
-					if(Board[sy + 1][sx - 1].first == 0){
-						return false;
-					}
-					else if(Board[sy + 1][sx - 1].first == Board[sy][sx].first){
-						return false;
-					}
-					else{
-						v_p[Board[sy+1][sx-1].second]->setStatus(false);
-						Board[sy+1][sx+1].first = 0;
-					}
-				}
-			}
+			else{ //jumping over >2 spots
+				return false;
+			}	
 			//printf("1");		
 			//printf("2");	
 			
